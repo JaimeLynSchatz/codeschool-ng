c60adc5224fc84a2902af8da0f557d9df2d6f39c
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-10-08 11:44:49 -0700

    add initial structure for tabs

--- app.js
+++ app.js
@@ -2,13 +2,27 @@
 	var app = angular.module('store', []);
 
 	app.controller('StoreController', function() {
-
+	this.products = gems;
 	});
 
-	var gem = {
+	var gems = [
+	{
 		name: 'Dodecahedron',
 		price: 2.95,
 		description: ' ... this thing fucking rocks! ... ',
+		canPurchase: false,
+		soldOUt: true,
+		images: ["http://placekitten.com/200/200"]
+	},
+	
+	{
+		name: 'Dodecahedron2',
+		price: 12.95,
+		description: ' ... Wow! this thing fucking rocks! ... ',
+		canPurchase: true,
+		soldOUt: false,
+		images: ["http://placekitten.com/200/200"]
 	}
+	];
 
 })();
